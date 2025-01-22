50e18eb77d9d879e14486f94d5820a6502eb9e36
BraisK <braiscasares26@gmail.com>
2025-01-22 12:05:23 +0100

    Login y register

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 npm init -y 
 npm i -D typescript tsx @types/node
 npx tsc --init
-configurar .swcrc y tsconfig 
+# configurar .swcrc y tsconfig 
 
 npm i express
 npm i -D @types/express
@@ -10,9 +10,14 @@ npm i -D @types/express
 npm i -D @swc/core @swc/cli 
 
 npm i prisma @prisma/client
-change package.json(prisma init)
-npm run prisma:init 
+# change package.json(prisma init)
+npm run prisma:init #(y los otros 2)
 
+npm i bcrypt
+npm i -D @types/bcrypt 
+
+npm i  jsonwebtoken
+npm i -D @types/jsonwebtoken
 
 
 ```
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,29 @@
-export class AuthController{
-    static register(req:Request, res:Response){
+import { AuthService } from "../services/auth.service";
+import {Response, Request} from 'express'
 
+
+export class AuthController{
+    static async register(req:Request, res:Response){
+        try {    
+            const userData = req.body  
+            //TODO validar el 
+            const newUser = await AuthService.register(userData)
+            res.status(201).json({message:'User register successfully', newUser})
+        } catch (error) {
+            res.status(409).json({message:'Fallo al registrar al usuario'})
+        }
+        
     }
 
-    static login(req:Request, res:Response){
-
+    static async login(req:Request, res:Response){
+        try {    
+            const userData = req.body  
+            //TODO validar el 
+            const token = await AuthService.login(userData.email, userData.password)
+            res.status(201).json({message:'Login successfully', token})
+        } catch (error) {
+            res.status(409).json({message:'Fallo al loguearse '+ error})
+        }
     }
 }
 
